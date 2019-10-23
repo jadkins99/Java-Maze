500f2d97f4e69ab5e034c8a124f18e2196da483d
Atalay Kutlay <atalaykutlaytr@gmail.com>
2019-10-23 18:26:50 -0400

    RandomPathFinder finished

--- MazeApplication.java
+++ MazeApplication.java
@@ -1,11 +1,15 @@
 import javafx.application.Application;
 import javafx.scene.Group;
-import javafx.scene.Scene;
+import javafx.scene.*;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
+
 import java.util.*;
+import javafx.event.*;
+
 import java.util.concurrent.TimeUnit;
 
 public class MazeApplication extends Application{
@@ -20,8 +24,15 @@ public class MazeApplication extends Application{
         maze_canvas = new Canvas(1200, 800);
  
         root.getChildren().add(maze_canvas);
-        primaryStage.setScene(new Scene(root, 1200, 800, Color.WHITE));
+        Scene mazeScene = new Scene(root, 1200, 800, Color.WHITE);
+        primaryStage.setScene(mazeScene);
         primaryStage.show();
+        mazeScene.addEventFilter(MouseEvent.MOUSE_PRESSED, new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                animateSolution();
+            }
+        });
 
 	    displayTheMaze();
     }
@@ -31,6 +42,10 @@ public class MazeApplication extends Application{
     void displayTheMaze() {
     	the_maze = new MazeAdapter();
     	MazeDisplayGraphics.display(the_maze, maze_canvas);
+                
+    }
+
+    void animateSolution() {
         DirType dir;
         int x = 0, y = 0;
         Thing crazyMonster = the_maze.getThing(0,0);
@@ -39,17 +54,12 @@ public class MazeApplication extends Application{
         path = crazyMonster.getMove();
 
         ListIterator<DirType> iterator = path.listIterator();
-        long mls = System.currentTimeMillis();
         while( iterator.hasNext() ) {
-            if(System.currentTimeMillis() - mls > 1000) { 
             crazyMonster.move(iterator.next());
             System.out.println("moved");
             MazeDisplayGraphics.display(the_maze, maze_canvas);
-            mls = System.currentTimeMillis();
-            }
+            
         }
-
-        
     }
 
 }
